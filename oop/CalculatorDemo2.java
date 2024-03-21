9a363c9dd5519356a895ba576290f91cfd070411
JongChan Lee <114732330+jjsok73379@users.noreply.github.com>
2024-03-21 18:34:38 +0900

    복습
    복습

---
+++ oop/CalculatorDemo2.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class CalculatorDemo2 {
+
+    public static void main(String[] args) {
+        sum(10, 20);
+        sum(20, 40);
+    }
+
+    public static void sum(int left, int right) {
+        System.out.println(left + right);
+    }
+}
